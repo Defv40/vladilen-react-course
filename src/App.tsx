fcbf171880fc69c745e793501791967edc7cadc8
German Popov <ProgDefv@gmail.com>
2022-12-30 16:49:41 +0400

    Feat: (add tailwindCss)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import './App.css';
+
 
 function App() {
-  return (
-  <h1>Hello world</h1>
-  );
+  return(
+    <h1 className='text-3xl font-bold'>Hello world</h1>
+  )
 }
 
 export default App;
